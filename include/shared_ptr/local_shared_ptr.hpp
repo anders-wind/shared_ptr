331d948eea00248753e3aa56ae2af0340b9d4ae3
Anders Wind <awia00@gmail.com>
2022-06-07 00:10:01 +0200

    refactored to only have one control block

--- include/shared_ptr/local_shared_ptr.hpp
+++ include/shared_ptr/local_shared_ptr.hpp
@@ -2,89 +2,10 @@
 #include <memory>
 #include <type_traits>
 
+#include <shared_ptr/control_block.hpp>
+
 namespace wind::local
 {
-namespace detail
-{
-template<typename T>
-struct control_block
-{
-    T* data;
-    size_t counter;
-
-    explicit control_block(T* i_data) noexcept
-        : data {i_data}
-        , counter {1}
-    {
-    }
-
-    control_block(const control_block&) noexcept = default;
-    control_block(control_block&&) noexcept = default;
-    auto operator=(const control_block&) noexcept -> control_block& = default;
-    auto operator=(control_block&&) noexcept -> control_block& = default;
-
-    virtual ~control_block() = default;
-
-    void inc() noexcept
-    {
-        this->counter++;
-    }
-
-    [[nodiscard]] auto decrement_and_check_zero() noexcept -> bool
-    {
-        return --this->counter == 0;
-    }
-};
-
-template<typename T, typename DeleterF>
-struct control_block_with_deleter final : control_block<T>
-{
-    DeleterF deleter;
-
-    control_block_with_deleter(T* i_data, DeleterF i_deleter) noexcept
-        : control_block<T>(i_data)
-        , deleter {std::move(i_deleter)}
-    {
-    }
-
-    control_block_with_deleter(const control_block_with_deleter&) noexcept = default;
-    control_block_with_deleter(control_block_with_deleter&&) noexcept = default;
-    auto operator=(const control_block_with_deleter&) noexcept -> control_block_with_deleter& = default;
-    auto operator=(control_block_with_deleter&&) noexcept -> control_block_with_deleter& = default;
-
-    ~control_block_with_deleter() noexcept override
-    {
-        this->deleter(this->data);
-    };
-};
-
-template<typename T, typename DeleterF>
-auto new_control_block_with_deleter(T* ptr, DeleterF&& deleter)
-{
-    return new control_block_with_deleter<T, DeleterF>(ptr, std::forward<DeleterF>(deleter));  // NOLINT
-}
-
-template<typename T>
-struct control_block_with_data final : control_block<T>
-{
-    T val;
-
-    template<typename... Args>
-    explicit control_block_with_data(Args&&... args) noexcept
-        : control_block<T>(&this->val)
-        , val {std::forward<Args>(args)...}
-    {
-    }
-};
-
-template<typename T, typename... Args>
-auto new_control_block_with_data(Args&&... args)
-{
-    return new control_block_with_data<T>(std::forward<Args>(args)...);  // NOLINT
-}
-
-}  // namespace detail
-
 template<typename T>
 struct shared_ptr
 {
@@ -92,23 +13,23 @@ struct shared_ptr
     using counter_type = size_t;
 
   private:
-    detail::control_block<T>* control_block_ {nullptr};
+    detail::control_block<T, std::size_t>* control_block_ {nullptr};
 
   public:
     shared_ptr() = default;
 
     explicit shared_ptr(element_type* ptr)
-        : control_block_(detail::new_control_block_with_deleter(ptr, std::default_delete<element_type>()))
+        : control_block_(detail::new_control_block_with_deleter<std::size_t>(ptr, std::default_delete<element_type>()))
     {
     }
 
     template<typename DeleterF>
     shared_ptr(element_type* ptr, DeleterF&& deleter)
-        : control_block_(detail::new_control_block_with_deleter<element_type>(ptr, std::forward<DeleterF>(deleter)))
+        : control_block_(detail::new_control_block_with_deleter<std::size_t>(ptr, std::forward<DeleterF>(deleter)))
     {
     }
 
-    explicit shared_ptr(detail::control_block<element_type>* control_block)
+    explicit shared_ptr(detail::control_block<element_type, std::size_t>* control_block)
         : control_block_(control_block)
     {
     }
@@ -230,7 +151,8 @@ auto make_shared(Args&&... args) -> shared_ptr<typename std::remove_extent_t<T>>
 {
     using element_type = typename std::remove_extent_t<T>;
 
-    return shared_ptr<element_type> {detail::new_control_block_with_data<element_type>(std::forward<Args>(args)...)};
+    return shared_ptr<element_type> {
+        detail::new_control_block_with_data<element_type, std::size_t>(std::forward<Args>(args)...)};
 }
 
 }  // namespace wind::local
